ecbb4cc663a931fd66bc65cde2c93d763d37e795
herreang000 <herreang000@warren.k12.in.us>
2025-02-19 11:13:12 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Student> student = new ArrayList<>();
+
+
+        Student mark = new Student("Mark", 3.87);
+        Student linda = new Student("Linda", 4.12);
+        Student clarence = new Student("Clarence", 3.93);
+
+        Collections.sort(student);
+        System.out.println(student);
+
+
+    }
 }
